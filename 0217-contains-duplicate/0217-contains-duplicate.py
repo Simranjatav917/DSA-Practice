614f224e4b5c202307892a5789f495d0986acafc
Simranjatav917 <110756496+Simranjatav917@users.noreply.github.com>
2025-09-15 17:24:14 +0530

    Time: 11 ms (89.33%), Space: 24.6 MB (84.54%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,14 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        nums.sort()
-        for i in range (len(nums)-1):
-            if nums[i]==nums[i+1]:
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        my_hash_map = {}
+        for num in nums:
+            if num in my_hash_map:
                 return True
-        return False        
+            my_hash_map[num] = True
+        return False
 
         
