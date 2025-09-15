ce035c02341a3a54e7c5fd84b60424ed1e5feea6
Simranjatav917 <110756496+Simranjatav917@users.noreply.github.com>
2025-09-15 10:13:31 +0530

    Time: 29 ms (80.07%), Space: 23.9 MB (74.1%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -9,3 +9,21 @@ class Solution(object):
             seen[num] = i
             
         return False
+
+#   fruits = ['apple', 'banana', 'cherry']
+
+# # Using enumerate() to get both index and value
+# for index, fruit in enumerate(fruits):
+#     print(f"Index: {index}, Fruit: {fruit}")
+
+# Output:
+# Index: 0, Fruit: apple
+# Index: 1, Fruit: banana
+# Index: 2, Fruit: cherry
+
+# fruits = ['apple', 'banana', 'cherry']
+# index = 0
+
+# for fruit in fruits:
+#     print(f"Index: {index}, Fruit: {fruit}")
+#     index += 1
